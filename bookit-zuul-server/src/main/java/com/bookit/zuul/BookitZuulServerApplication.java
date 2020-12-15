3d62bd70955a757101c59e8ab29d54eb201746fc
Hitesh Abichandani <offabichandani@gmail.com>
2020-12-15 12:57:43 +0530

    Configure Zuul Server

--- bookit-zuul-server/src/main/java/com/bookit/zuul/BookitZuulServerApplication.java
+++ bookit-zuul-server/src/main/java/com/bookit/zuul/BookitZuulServerApplication.java
@@ -2,8 +2,13 @@ package com.bookit.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+//@EnableOAuth2Sso
 public class BookitZuulServerApplication {
 
 	public static void main(String[] args) {
