1393dc289d11ee82d339f7e2a01a07bd1aace59a
Hitesh Abichandani <offabichandani@gmail.com>
2020-12-15 19:29:05 +0530

    Configure Discovery Server

--- bookit-discovery-server/src/main/java/com/bookit/discovery/BookitDiscoveryServerApplication.java
+++ bookit-discovery-server/src/main/java/com/bookit/discovery/BookitDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.bookit.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BookitDiscoveryServerApplication {
 
 	public static void main(String[] args) {
